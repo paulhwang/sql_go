db9acce29681a43202b676a9fa8460cc8aa487e7
phwang <phwang@gmail.com>
2021-04-21 02:03:46 -0700

    init x

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+ ******************************************************************************
+ *                                       
+ *  Copyright (c) 2021 phwang. All rights reserved.
+ *
+ ******************************************************************************
+ */
+
+#include <stdio.h>
+#include "root_class.h"
+
+root_class *root;
+
+int main() {
+	printf("hello phwang\n");
+	root = new root_class;
+}
---
+++ root_class.cpp
@@ -0,0 +1,17 @@
+/*
+ ******************************************************************************
+ *                                       
+ *  Copyright (c) 2021 phwang. All rights reserved.
+ *
+ ******************************************************************************
+ */
+
+#include <stdio.h>
+#include "root_class.h"
+
+root_class::root_class() {
+	printf("root\n");
+}
+
+root_class::~root_class() {
+}
