b49d591d2b4dbd6dcbe0e95f9057a5a11dfc77d7
Paul Hwang <phwang@gmail.com>
2021-04-22 06:59:07 -0700

    raw

--- core/raw_api_class.cpp
+++ core/raw_api_class.cpp
@@ -7,6 +7,7 @@
  */
 
 #include <stdio.h>
+#include "../sqlite/sqlite3.h"
 #include "raw_api_class.h"
 
 RawApiClass::RawApiClass() {
@@ -16,42 +17,47 @@ RawApiClass::RawApiClass() {
 RawApiClass::~RawApiClass() {
 }
 
-void RawApiClass::sqlite3() {
-
+int RawApiClass::sqlite3Open(const char *filename, sqlite3 **ppDb) {
+	return sqlite3_open(filename, ppDb);
 }
 
-void RawApiClass::sqlite3_stmt() {
-
+int RawApiClass::sqlite3Prepare(sqlite3 *db, const char *zSql, int nByte, sqlite3_stmt **ppStmt, const char **pzTail) {
+	return sqlite3_prepare(db, zSql, nByte, ppStmt, pzTail);
 }
 
-void RawApiClass::sqlite3_open() {
-
+int RawApiClass::sqlite3BindInt(sqlite3_stmt* stmt_val, int int_val1, int int_val2) {
+	return sqlite3_bind_int(stmt_val, int_val1, int_val2);
 }
 
-void RawApiClass::slqite3_prepare() {
-
+int RawApiClass::sqlite3BindNull(sqlite3_stmt* stmt_val, int int_val) {
+	return sqlite3_bind_null(stmt_val, int_val);
 }
 
-void RawApiClass::sqlite3_bind() {
-
+int RawApiClass::sqlite3BindText(sqlite3_stmt* stmt_val, int int_val1, const char* str_val, int int_val2, void(*)(void*)) {
+	return sqlite3_bind_text(stmt_val, int_val1, str_val, int_val2, SQLITE_TRANSIENT);
 }
 
-void RawApiClass::sqlite3_step() {
-
+int RawApiClass::sqlite3Step(sqlite3_stmt* stmt_val) {
+	return sqlite3_step(stmt_val);
 }
 
-void RawApiClass::sqlite3_column() {
-
+int RawApiClass::sqlite3ColumnInt(sqlite3_stmt* stmt_val, int iCol) {
+	return sqlite3ColumnInt(stmt_val, iCol);
 }
 
-void RawApiClass::sqlite3_finalize() {
-
+const unsigned char *RawApiClass::sqlite3ColumnText(sqlite3_stmt* stmt_val, int iCol) {
+	return sqlite3_column_text(stmt_val, iCol);
 }
 
-void RawApiClass::sqlite3_close() {
 
+int RawApiClass::sqlite3Finalize(sqlite3_stmt *pStmt) {
+	return sqlite3_finalize(pStmt);
 }
 
-void RawApiClass::sqlite3_exec() {
-	
+int RawApiClass::sqlite3Close(sqlite3* sqlite3_val) {
+	return sqlite3_close(sqlite3_val);
+}
+
+int RawApiClass::sqlite3Exec(sqlite3* sqlite3_val, const char *sql, int (*callback)(void*,int,char**,char**), void * val, char **errmsg) {
+	return sqlite3_exec(sqlite3_val, sql, callback, val, errmsg);
 }
--- core/raw_api_class.h
+++ core/raw_api_class.h
@@ -6,19 +6,39 @@
  ******************************************************************************
  */
 
+#include "../sqlite/sqlite3.h"
 
 class RawApiClass {
 public:
 	RawApiClass();
 	~RawApiClass();
-	void sqlite3();
-	void sqlite3_stmt();
-	void sqlite3_open();
-	void slqite3_prepare();
-	void sqlite3_bind();
-	void sqlite3_step();
-	void sqlite3_column();
-	void sqlite3_finalize();
-	void sqlite3_close();
-	void sqlite3_exec();
+
+	int sqlite3Open(
+		const char *filename,   /* Database filename (UTF-8) */
+		sqlite3 **ppDb          /* OUT: SQLite db handle */
+	);
+
+	int sqlite3Prepare(
+		sqlite3 *db,            /* Database handle */
+  		const char *zSql,       /* SQL statement, UTF-8 encoded */
+  		int nByte,              /* Maximum length of zSql in bytes. */
+  		sqlite3_stmt **ppStmt,  /* OUT: Statement handle */
+  		const char **pzTail     /* OUT: Pointer to unused portion of zSql */
+	);
+
+	int sqlite3BindInt(sqlite3_stmt*, int, int);
+	int sqlite3BindNull(sqlite3_stmt*, int);
+	int sqlite3BindText(sqlite3_stmt*, int, const char*, int, void(*)(void*));
+	int sqlite3Step(sqlite3_stmt*);
+	int sqlite3ColumnInt(sqlite3_stmt*, int iCol);
+	const unsigned char *sqlite3ColumnText(sqlite3_stmt*, int iCol);
+	int sqlite3Finalize(sqlite3_stmt *pStmt);
+	int sqlite3Close(sqlite3*);
+	int sqlite3Exec(
+		sqlite3*,                                  /* An open database */
+		const char *sql,                           /* SQL to be evaluated */
+		int (*callback)(void*,int,char**,char**),  /* Callback function */
+		void *,                                    /* 1st argument to callback */
+		char **errmsg                              /* Error msg written here */
+		);
 };
