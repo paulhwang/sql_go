bc0fd12016984f01b511750ba23694f7edd14cfa
Paul Hwang <phwang@gmail.com>
2021-04-26 05:39:55 -0700

    test

--- test_dir/test_class.cpp
+++ test_dir/test_class.cpp
@@ -7,13 +7,17 @@
  */
 
 #include <stdio.h>
+#include "../utils_dir/utils_include.h"
 #include "../utils_dir/utils_class.h"
 #include "../utils_dir/abend_dir/abend_class.h"
 #include "../sql_server_dir/sql_server_raw_api_class.h"
 #include "test_class.h"
 
 TestClass::TestClass(SqlServerRootClass *root_class_val) {
+    memset(this, 0, sizeof (*this));
+    this->debugSwitchOn_ = true;
     this->debug(true, "TestClass", "init");
+
     this->rootClass_ = root_class_val;
 
 }
@@ -28,19 +32,22 @@ void TestClass::doTest() {
 
 }
 
-AbendClass *TestClass::abendObject() { return this->utilsObject()->abendObject(); }
-void TestClass::log(const char *s0, const char *s1) { char buf[1048]; sprintf(buf, "%s.%s()", this->objectName(), s0); this->logIt(buf, s1); }
-void TestClass::abend(const char *s0, const char *s1) { char buf[1048]; sprintf(buf, "%s.%s()", this->objectName(), s0); this->abendIt(buf, s1); }
+void TestClass::log(const char *s0, const char *s1) {
+    char buf[UTILS_DEFINE_ABEND_BUF_SIZE]; sprintf(buf, "%s.%s()", this->objectName(), s0); this->logIt(buf, s1);
+}
+
+void TestClass::abend(const char *s0, const char *s1) {
+    char buf[UTILS_DEFINE_ABEND_BUF_SIZE]; sprintf(buf, "%s.%s()", this->objectName(), s0); this->abendIt(buf, s1);
+}
+
 void TestClass::logIt(const char *s0, const char *s1) {
-	if (this->debugSwitchOn_) {
-		AbendClass::sLog(s0, s1);
-	}
+    if (this->debugSwitchOn_) {
+        AbendClass::sLog(s0, s1);
+    }
 }
 
 void TestClass::abendIt(const char *s0, const char *s1) {
-	if (this->debugSwitchOn_) {
-		AbendClass::sAbend(s0, s1);
-	}
+    if (this->debugSwitchOn_) {
+        AbendClass::sAbend(s0, s1);
+    }
 }
-
-
